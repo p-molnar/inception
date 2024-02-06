eeac0798a9708e251e87c67127e2c41eca4151e0
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2024-02-06 16:57:26 +0100

    Remove root's password resetting

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -4,16 +4,21 @@ db_name=db
 db_user=pmolnar
 db_pwd=abc123abc
 
-service mysql start 
+service mariadb start
 
-echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db.sql
-echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db.sql
-echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db.sql
-echo "FLUSH PRIVILEGES;" >> db.sql
+until mysqladmin ping -hlocalhost --silent; do
+    echo "Waiting for MySQL service to start..."
+    sleep 5
+done
 
-mysql < db.sql
+echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > init_db.cmd
+echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> init_db.cmd
+echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> init_db.cmd
+# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> init_db.cmd
+echo "FLUSH PRIVILEGES;" >> init_db.cmd
 
-kill $(cat /var/run/mysqld/mysqld.pid)
+mysql -u root < init_db.cmd
+
+# kill $(cat /var/run/mysqld/mysqld.pid)
 
 mysqld
