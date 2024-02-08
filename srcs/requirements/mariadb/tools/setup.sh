f5f8591cf3b97be962ee32fde439aadf343429c9
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2024-02-08 22:19:34 +0100

    Rename file; Update bash script

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-db_name=db
-db_user=pmolnar
-db_pwd=abc123abc
-
-service mariadb start
+service mysql start
 
 until mysqladmin ping -hlocalhost --silent; do
     echo "Waiting for MySQL service to start..."
     sleep 5
 done
 
-echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > init_db.cmd
-echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> init_db.cmd
-echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> init_db.cmd
+DB_NAME=test
+DB_USER=test_user
+DB_PWD=123
+
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > init_db.cmd
+echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD' ;" >> init_db.cmd
+echo "GRANT ALL PRIVILEGES ON $DB_USER.* TO '$DB_USER'@'%' ;" >> init_db.cmd
 # echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> init_db.cmd
 echo "FLUSH PRIVILEGES;" >> init_db.cmd
 
-mysql -u root < init_db.cmd
+mysql < init_db.cmd
 
-# kill $(cat /var/run/mysqld/mysqld.pid)
+sleep 3
+
+service mysql stop
 
 mysqld
