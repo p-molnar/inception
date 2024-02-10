23dcd51a77915eb853ee4682b1cff62f41ca3212
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2024-02-10 21:11:50 +0100

    Fix bugs

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -6,24 +6,23 @@ echo "server {
     listen 443 ssl;
     listen [::]:443 ssl;
 
+    ssl_protocols TLSv1.3;
     ssl_certificate $SSL_CERTS_PATH/$DOMAIN_NAME.crt;
     ssl_certificate_key $SSL_KEY_PATH/$DOMAIN_NAME.key;
-	ssl_protocols TLSv1.3;
 	
-	root /var/www/html;
-    index index.php
+    root /var/www/html;
+    index index.php index.nginx-debian.html;
 
-	server_name $DOMAIN_NAME www.$DOMAIN_NAME;
+    server_name $DOMAIN_NAME www.$DOMAIN_NAME;
 
-    location ~ \.php$ {
-    fastcgi_split_path_info ^(.+\.php)(/.+)$;
-    fastcgi_pass wordpress:9000;
-    fastcgi_index index.php;
-    include fastcgi_params;
-    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-    fastcgi_param SCRIPT_NAME $fastcgi_script_name;
-  }
- 
+    location ~ [^/]\\.php(/|$) {
+        try_files \$uri =404;
+        fastcgi_pass wordpress:9000;
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+    }
 }" > /etc/nginx/sites-available/default
+
+# service nginx start
 
 nginx -g "daemon off;"
